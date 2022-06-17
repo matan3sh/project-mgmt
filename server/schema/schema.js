f44b39c398941e19365453da2998792b6bedb3ee
Matan Shaviro <matan3sh@gmail.com>
2022-06-17 11:08:11 +0300

    Connect the app to Mongo Database

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,6 +8,10 @@ const {
   GraphQLSchema,
 } = require("graphql");
 
+// Mongoose models
+const Client = require("../models/Client");
+const Project = require("../models/Project");
+
 // Client Type
 const ClientType = new GraphQLObjectType({
   name: "Client",
@@ -30,7 +34,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
       },
     },
   }),
@@ -42,27 +46,27 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve() {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve() {
-        return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
   },
